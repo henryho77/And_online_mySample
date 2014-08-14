189b1830dd7a13a2c80d4872ce9a4ec53c5faf12
henryho <squirrelho@gmail.com>
2014-08-14 19:15:48 +0800

    FragmentA,B associate with fragment_a,b

--- mySample_InterFragment/mySample_InterFragment/src/com/example/mysample_interfragment/FragmentA.java
+++ mySample_InterFragment/mySample_InterFragment/src/com/example/mysample_interfragment/FragmentA.java
@@ -1,7 +1,18 @@
 package com.example.mysample_interfragment;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class FragmentA extends Fragment{
-
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View rootView = inflater.inflate(R.layout.fragment_a, container, false);
+		
+		return rootView;
+	}
 }
