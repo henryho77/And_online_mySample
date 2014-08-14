96a530b1ed9dff8436ec914e26dbfb58e7f1c907
henryho <squirrelho@gmail.com>
2014-08-14 16:05:09 +0800

    something modified

--- mySample_Frag01/mySample_Frag01/src/com/example/mysample_frag01/MyFragment.java
+++ mySample_Frag01/mySample_Frag01/src/com/example/mysample_frag01/MyFragment.java
@@ -1,5 +1,18 @@
 package com.example.mysample_frag01;
 
-public class MyFragment {
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+public class MyFragment extends Fragment{
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View rootView = inflater.inflate(R.layout.my_fragment_layout, container, false);
+		
+		return rootView;
+	}
 }
