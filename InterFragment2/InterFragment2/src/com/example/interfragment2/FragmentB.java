820770df5aa9b61b9ef59a5ed6f03326d7265f3d
henryho <squirrelho@gmail.com>
2014-08-15 11:55:54 +0800

    add FragmentA,B & fragment_a,b and modify something

---
+++ InterFragment2/InterFragment2/src/com/example/interfragment2/FragmentB.java
@@ -0,0 +1,19 @@
+package com.example.interfragment2;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentB extends Fragment{
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View rootView = inflater.inflate(R.layout.fragment_b, container, false);
+		
+		return rootView;
+	}
+}
