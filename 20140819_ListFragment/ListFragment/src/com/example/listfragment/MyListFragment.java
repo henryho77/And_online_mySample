394f5fe8e63d21a0f5264b77daa2663bd10c6b81
henryho <squirrelho@gmail.com>
2014-08-19 11:37:34 +0800

    MyListFragment implements AdapterView.OnItemClickListener

--- 20140819_ListFragment/ListFragment/src/com/example/listfragment/MyListFragment.java
+++ 20140819_ListFragment/ListFragment/src/com/example/listfragment/MyListFragment.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
-public class MyListFragment extends ListFragment {
+
+public class MyListFragment extends ListFragment implements AdapterView.OnItemClickListener{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -24,6 +27,15 @@ public class MyListFragment extends ListFragment {
 		
 		ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.heroes, android.R.layout.simple_list_item_1);
 		setListAdapter(adapter);
+		getListView().setOnItemClickListener(this);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+		// TODO Auto-generated method stub
+		
+		Toast.makeText(getActivity(), "Item "+ position, Toast.LENGTH_SHORT).show();
 	}
 	
 	
