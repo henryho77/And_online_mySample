ee284bc645076aabf5cf0a36b3b4567af138fd47
henryho <squirrelho@gmail.com>
2014-08-19 16:41:29 +0800

    delete setItems, add setMultiChoiceItems

--- 20140819-3_AlertDialog/AlertDialog/src/com/example/alertdialog/MyAlert.java
+++ 20140819-3_AlertDialog/AlertDialog/src/com/example/alertdialog/MyAlert.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -16,12 +17,14 @@ public class MyAlert extends DialogFragment{
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 		
 		builder.setTitle("My Dialog");
-		builder.setItems(R.array.days, new OnClickListener() {
+		builder.setMultiChoiceItems(R.array.days, null, new OnMultiChoiceClickListener() {
 			
 			@Override
-			public void onClick(DialogInterface dialog, int which) {
+			public void onClick(DialogInterface dialog, int which, boolean isChecked) {
 				// TODO Auto-generated method stub
-				Toast.makeText(getActivity(), "Item was selected " + which, Toast.LENGTH_SHORT).show();
+				Toast.makeText(getActivity(), 
+						"Item from position "+ which + " was selected " + isChecked,
+						Toast.LENGTH_SHORT).show();
 			}
 		});
 		
