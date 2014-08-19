a36e0ad637601c9b4a3e4d861504126edc2e17c3
henryho <squirrelho@gmail.com>
2014-08-19 16:11:19 +0800

    set properties to dialog: Title, Message, Negative & PositiveButton

--- 20140819-3_AlertDialog/AlertDialog/src/com/example/alertdialog/MyAlert.java
+++ 20140819-3_AlertDialog/AlertDialog/src/com/example/alertdialog/MyAlert.java
@@ -3,7 +3,10 @@ package com.example.alertdialog;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MyAlert extends DialogFragment{
 	
@@ -11,6 +14,28 @@ public class MyAlert extends DialogFragment{
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		
+		builder.setTitle("My Dialog");
+		builder.setMessage("Do you guys like this dialog?");
+		
+		builder.setNegativeButton(android.R.string.cancel, new OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getActivity(), "Negative button was clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
+
+		builder.setPositiveButton(android.R.string.ok, new OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getActivity(), "Positive button was clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
 		Dialog dialog = builder.create();
 		
 		return dialog;
