4b9d9a45887661bdfc0189d8a25a3423a77bceb8
henryho <squirrelho@gmail.com>
2014-08-19 11:27:08 +0800

    modify textView id, and use ArrayAdapter

--- 20140819_ListFragment/ListFragment/src/com/example/listfragment/MyListFragment.java
+++ 20140819_ListFragment/ListFragment/src/com/example/listfragment/MyListFragment.java
@@ -1,7 +1,30 @@
 package com.example.listfragment;
 
 import android.app.ListFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
-public class MyListFragment extends ListFragment{
-
+public class MyListFragment extends ListFragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View rootView = inflater.inflate(R.layout.my_list_fragment, container, false);
+		
+		return rootView;
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onActivityCreated(savedInstanceState);
+		
+		ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.heroes, android.R.layout.simple_list_item_1);
+		setListAdapter(adapter);
+	}
+	
+	
 }
