3e678228ebc5580be9b012b1601fd58d727f70f4
henryho <squirrelho@gmail.com>
2014-08-20 11:20:59 +0800

    FragmentPagerAdapter =>FragmentStatePagerAdapter
    本次觀察重點:
    從FragA=>B=>C
    onSaveInstanceState這次會被呼叫了,所以資料可以暫存在此.
    這次多了onSaveInstanceState,onDetach,以及往回拉的時候onCreate SUBSEQUENT TIME.
    順序為:
    onAttach,
    onCreate FIRST TIME,
    onCreateView,
    onActivityCreated,
    onStart,
    onResume,
    "onSaveInstanceState",
    onPause,
    onStop,
    onDestroyView,
    onDestroy,
    "onDetach"

--- 20140820_ScrollTabs2/ScrollTabs/src/com/example/scrolltabs/MainActivity.java
+++ 20140820_ScrollTabs2/ScrollTabs/src/com/example/scrolltabs/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import android.os.Bundle;
@@ -29,7 +30,7 @@ public class MainActivity extends FragmentActivity{
 
 }
 
-class MyAdapter extends FragmentPagerAdapter
+class MyAdapter extends FragmentStatePagerAdapter
 {
 
 	public MyAdapter(FragmentManager fm) {
