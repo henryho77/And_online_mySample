e9568c45b21db89cc52a90cb6bd2c7468a705de4
henryho <squirrelho@gmail.com>
2014-08-20 10:43:53 +0800

    new project ScrollTabs2 (copy ScrollTabs)

---
+++ 20140820_ScrollTabs2/ScrollTabs/src/com/example/scrolltabs/FragmentA.java
@@ -0,0 +1,19 @@
+package com.example.scrolltabs;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentA extends Fragment{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater,
+			@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		View rootView = inflater.inflate(R.layout.fragment_a, container, false);
+		return rootView;
+	}
+}
