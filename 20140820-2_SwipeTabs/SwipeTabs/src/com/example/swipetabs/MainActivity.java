5b0da84bf95c37fefb9da8cf439e2662a702df66
henryho <squirrelho@gmail.com>
2014-08-20 13:51:14 +0800

    deal with ActionBar and TabListener

--- 20140820-2_SwipeTabs/SwipeTabs/src/com/example/swipetabs/MainActivity.java
+++ 20140820-2_SwipeTabs/SwipeTabs/src/com/example/swipetabs/MainActivity.java
@@ -1,19 +1,60 @@
 package com.example.swipetabs;
 
-import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.FragmentActivity;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements TabListener{
 
+	ActionBar actionBar;
+	
 	@Override
 	protected void onCreate(Bundle arg0) {
 		// TODO Auto-generated method stub
 		super.onCreate(arg0);
 		setContentView(R.layout.activity_main);
+		
+		actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		ActionBar.Tab tab1 = actionBar.newTab();
+		tab1.setText("Tab 1");
+		tab1.setTabListener(this);
+		
+		ActionBar.Tab tab2 = actionBar.newTab();
+		tab2.setText("Tab 2");
+		tab2.setTabListener(this);
+		
+		ActionBar.Tab tab3 = actionBar.newTab();
+		tab3.setText("Tab 3");
+		tab3.setTabListener(this);
+		
+		actionBar.addTab(tab1);
+		actionBar.addTab(tab2);
+		actionBar.addTab(tab3);
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		Log.d("debug", "onTabReselected at " + " position " + tab.getPosition() + " name " + tab.getText());
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		Log.d("debug", "onTabReselected at " + " position " + tab.getPosition() + " name " + tab.getText());
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		Log.d("debug", "onTabReselected at " + " position " + tab.getPosition() + " name " + tab.getText());
 	}
 
 }
