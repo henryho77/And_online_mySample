480657e3990985b862981feba0924e950ba8afbb
henryho <squirrelho@gmail.com>
2014-09-06 17:13:37 +0800

    [App2] add editText, button, textView

--- 20140906_ProcessShareApp/ProcessShareApp2/src/com/example/processshareapp2/MainActivity.java
+++ 20140906_ProcessShareApp/ProcessShareApp2/src/com/example/processshareapp2/MainActivity.java
@@ -4,33 +4,31 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
+	private EditText edt_messageFromApp1;
+	private TextView txt_status;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        edt_messageFromApp1 = (EditText) findViewById(R.id.edt_messageFromApp1);
+        txt_status = (TextView) findViewById(R.id.txt_status);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public void loadFile(View view){
+    	
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    
+    public void readFile(String fullPath){
+    	
     }
+    
 }
