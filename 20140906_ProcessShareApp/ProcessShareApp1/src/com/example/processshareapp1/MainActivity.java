1f0d7ec63c333a72450ff4238fb1addd2dfd6aba
henryho <squirrelho@gmail.com>
2014-09-06 17:33:31 +0800

    [App1] saveFile

--- 20140906_ProcessShareApp/ProcessShareApp1/src/com/example/processshareapp1/MainActivity.java
+++ 20140906_ProcessShareApp/ProcessShareApp1/src/com/example/processshareapp1/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.processshareapp1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,7 +31,29 @@ public class MainActivity extends Activity {
     }
 
     public void saveFile(View view){
-    	
+    	File file = null;
+    	String text1 = edt_messageBox.getText().toString();
+    	FileOutputStream fileOutputStream = null;
+    	try {
+    		file = getFilesDir();
+    		fileOutputStream = openFileOutput("mytest.txt", Context.MODE_PRIVATE);
+    		fileOutputStream.write(text1.getBytes());
+    		txt_status.setTextColor(Color.GREEN);
+    		txt_status.setText(text1 + " \n written to\n " + file.getAbsolutePath());
+    	} catch (FileNotFoundException e){
+    		txt_status.setTextColor(Color.RED);
+    		txt_status.setText(e.toString());
+    	} catch (IOException e){
+    		txt_status.setTextColor(Color.RED);
+    		txt_status.setText(e.toString());
+    	} finally {
+    		try {
+    			fileOutputStream.close();
+    		} catch (IOException e){
+        		txt_status.setTextColor(Color.RED);
+        		txt_status.setText(e.toString());
+    		}
+    	}
     }
     
 }
