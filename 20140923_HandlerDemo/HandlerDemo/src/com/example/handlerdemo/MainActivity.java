101a1d514d11495c3a035bcaa18f589f5b3aa019
henryho <squirrelho@gmail.com>
2014-09-23 21:11:09 +0800

    create MyThread implements Runnable

--- 20140923_HandlerDemo/HandlerDemo/src/com/example/handlerdemo/MainActivity.java
+++ 20140923_HandlerDemo/HandlerDemo/src/com/example/handlerdemo/MainActivity.java
@@ -2,16 +2,35 @@ package com.example.handlerdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
 
+	Thread thread;
+	Handler handler;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /* here is run in main thread */
         setContentView(R.layout.activity_main);
+        
+        thread = new Thread(new MyThread());
+        thread.start();
+        
     }
 
+    class MyThread implements Runnable{
+
+		@Override
+		public void run() {
+			/* here is run in second thread */
+			for(int i = 0; i < 10000;i++){
+				
+			}
+		}	
+    }
 }
