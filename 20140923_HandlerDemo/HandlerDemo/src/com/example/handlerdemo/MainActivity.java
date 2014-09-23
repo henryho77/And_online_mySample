a372bea0bc4e47cf1496b327f212296b129d3284
henryho <squirrelho@gmail.com>
2014-09-23 21:16:57 +0800

    add Handler

--- 20140923_HandlerDemo/HandlerDemo/src/com/example/handlerdemo/MainActivity.java
+++ 20140923_HandlerDemo/HandlerDemo/src/com/example/handlerdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.handlerdemo;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -21,6 +22,14 @@ public class MainActivity extends Activity {
         thread = new Thread(new MyThread());
         thread.start();
         
+        //the handler object has been created inside the MainActivity
+        handler = new Handler(){
+        	@Override
+        	public void handleMessage(Message msg) {
+        		// TODO Auto-generated method stub
+        		super.handleMessage(msg);
+        	}
+        };
     }
 
     class MyThread implements Runnable{
@@ -28,8 +37,10 @@ public class MainActivity extends Activity {
 		@Override
 		public void run() {
 			/* here is run in second thread */
+
+			Message message = Message.obtain();
 			for(int i = 0; i < 10000;i++){
-				
+				handler.sendMessage(message);
 			}
 		}	
     }
