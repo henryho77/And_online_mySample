c217cea42e2aa71b13e64dc33fd337e64a5013bf
Henry Ho <squirrelho@gmail.com>
2016-03-11 16:33:40 +0800

    建立控件取得實體,並綁定監聽器

--- DownloadImage/Downloadimage/src/com/example/downloadimage/MainActivity.java
+++ DownloadImage/Downloadimage/src/com/example/downloadimage/MainActivity.java
@@ -1,19 +1,42 @@
 package com.example.downloadimage;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	private ImageView imageView1;
+	private Button btn_downloadImage;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        imageView1 = (ImageView) findViewById(R.id.imageView1);
+        btn_downloadImage = (Button) findViewById(R.id.btn_downloadImage);
+        
+        ButtonListener buttonListener = new ButtonListener();
+        btn_downloadImage.setOnClickListener(buttonListener);
     }
 
+    class ButtonListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+    	
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
