977b4f7400c163e8e6cc760fb280bd50ca32f928
Henry Ho <squirrelho@gmail.com>
2016-03-13 18:59:05 +0800

    取得實體 及 建立類別

--- UpdateUI/UpdateUI/src/com/example/updateui/MainActivity.java
+++ UpdateUI/UpdateUI/src/com/example/updateui/MainActivity.java
@@ -2,18 +2,61 @@ package com.example.updateui;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+	private TextView textView1;
+	private ProgressBar progressBar1;
+	private Button button1;
+	
+	private Handler handler;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        textView1 = (TextView) findViewById(R.id.textView1);
+        button1 = (Button) findViewById(R.id.button1);
+        progressBar1 = (ProgressBar) findViewById(R.id.progressBar1);
     }
 
+    class ButtonListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+    	
+    }
+    
+    class MyHandler extends Handler {
+    	@Override
+    	public void handleMessage(Message msg) {
+    		// TODO Auto-generated method stub
+    		super.handleMessage(msg);
+    	}
+    }
+    
+    class MyThread extends Thread {
+    	@Override
+    	public void run() {
+    		// TODO Auto-generated method stub
+    		super.run();
+    	}
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
